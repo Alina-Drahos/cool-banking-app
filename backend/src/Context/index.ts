a52a23fcb6bc5b9336e9a35c6cc19b3e125f5758
Alina Drahos <alinchen1066@outlook.com>
2024-05-16 20:51:47 -0500

    Removed comments

---
+++ backend/src/Context/index.ts
@@ -0,0 +1,17 @@
+
+export interface BankingRoutesLinkData {
+    linkSuccess: boolean;
+    isItemAccess: boolean;
+    isPaymentInitiation: boolean;
+    linkToken: string | null;
+    accessToken: string | null;
+    itemId: string | null;
+    isError: boolean;
+    backend: boolean;
+    products: string[];
+    linkTokenError: {
+      error_message: string;
+      error_code: string;
+      error_type: string;
+    };
+  }
--- backend/src/routes/BankingRoutes.ts
+++ backend/src/routes/BankingRoutes.ts
@@ -1,28 +1,25 @@
 import { IPublicToken } from '@src/models/Banking';
-import { IReq, IRes } from './types/express/misc'
+import { IReq, IRes } from './types/express/misc';
+import { BankingRoutesLinkData } from '@src/Context';
+
 
 require('dotenv').config();
 const { Configuration, PlaidApi, Products, PlaidEnvironments} = require('plaid');
 
 
-const APP_PORT = process.env.APP_PORT || 8000;
 const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID;
 const PLAID_SECRET = process.env.PLAID_SECRET;
-const PLAID_ENV = process.env.PLAID_ENV || 'sandbox';
+const PLAID_ENV = 'sandbox';
 
 // PLAID_PRODUCTS is a comma-separated list of products to use when initializing
 // Link. Note that this list must contain 'assets' in order for the app to be
 // able to create and retrieve asset reports.
-const PLAID_PRODUCTS = (process.env.PLAID_PRODUCTS || Products.Transactions).split(
-  ',',
-);
+const PLAID_PRODUCTS = ['auth,transactions'];
 
 
 // PLAID_COUNTRY_CODES is a comma-separated list of countries for which users
 // will be able to select institutions from.
-const PLAID_COUNTRY_CODES = (process.env.PLAID_COUNTRY_CODES || 'US').split(
-  ',',
-);
+const PLAID_COUNTRY_CODES = ['US,CA'];
 
 // Initialize the Plaid client
 // Find your API keys in the Dashboard (https://dashboard.plaid.com/account/keys)
@@ -39,12 +36,22 @@ const configuration = new Configuration({
 
 const client = new PlaidApi(configuration);
 
+// Generate a unique Guid for every session
+const guid =function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+  .replace(/[xy]/g, function (c) {
+      const r = Math.random() * 16 | 0, 
+          v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+  });
+}
+
 // Retrieve the Link Token from Plaid
 async function get(_: IReq, res: IRes) {
     const configs = {
       user: {
         // This should correspond to a unique id for the current user.
-        client_user_id: 'user-id',
+        client_user_id: guid()
       },
       client_name: 'Plaid Quickstart',
       products: PLAID_PRODUCTS,
@@ -53,7 +60,8 @@ async function get(_: IReq, res: IRes) {
     };
 
     const createTokenResponse = await client.linkTokenCreate(configs);
-    res.json(createTokenResponse.data);
+    const data = createTokenResponse.data as BankingRoutesLinkData;
+    res.json(data.linkToken);
   }
 
   async function put(req: IReq< IPublicToken>, res: IRes){
